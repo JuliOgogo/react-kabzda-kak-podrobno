2103d50ca9fa5d845fa6dc3597e49fa1418a4b32
Julia Titkova <juli.ogogo@yandex.ru>
2022-11-10 21:00:38 +0300

    10 - App component cleared

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion/Accordion";
-import {Rating, RatingValueType} from "./components/Rating/Rating";
-import {OnOff} from "./components/OnOff/OnOff";
-import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
-import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
-import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
 
 function App() {
     console.log("App rendering")
-
-    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
-    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
-    let [onOff, setOnOff] = useState<boolean>(false)
-
-    return (
-        <div className={"App"}>
-
-            <OnOff onClick={setOnOff}
-                   onOff={onOff}/>
-            <UncontrolledOnOff onChange={setOnOff}/> {onOff.toString()}
-
-            <Accordion titleValue={"Menu"}
-                       onClick={() => setAccordionCollapsed(!accordionCollapsed)}
-                       accordionCollapsed={accordionCollapsed}/>
-            <UncontrolledAccordion titleValue={"Users"}/>
-
-            <Rating value={ratingValue}
-                    onClick={setRatingValue}/>
-            <UncontrolledRating/>
-
-        </div>);
+    return <div className={"App"}>Some content</div>
 }
 
 export default App;

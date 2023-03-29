ff972327c12d9f6c69fd62fb00d969a67102e97e
Julia Titkova <juli.ogogo@yandex.ru>
2023-03-29 20:17:53 +0300

    21 - useEffect основы

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -9,18 +9,35 @@ export const UseEffectExample = () => {
     // debugger
 
     const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
 
     useEffect(() => {
-        console.log('Use Effect')
+        console.log('Use Effect every render')
         // debugger
         document.title = counter.toString()
     })
 
+    useEffect(() => {
+        console.log('Use Effect only first render')
+        // debugger
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('Use Effect when counter changed')
+        // debugger
+        document.title = counter.toString()
+    }, [counter])
+
     return <div>
-        Hello, {counter}
+        Hello, counter: {counter} fake: {fake}
         <button onClick={() => {
             setCounter(counter + 1)
-        }}>+
+        }}>Counter +
+        </button>
+        <button onClick={() => {
+            setFake(fake + 1)
+        }}>Fake +
         </button>
     </div>
 }

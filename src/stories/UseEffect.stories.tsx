611cc4be24f376fdfef266579340822a273f003e
Julia Titkova <juli.ogogo@yandex.ru>
2023-03-29 20:31:05 +0300

    22 - useEffect \ setTimeout

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -4,8 +4,8 @@ export default {
     title: 'Use Effect'
 }
 
-export const UseEffectExample = () => {
-    console.log('UseEffectExample')
+export const UseEffectBaseExample = () => {
+    console.log('UseEffectBaseExample')
     // debugger
 
     const [counter, setCounter] = useState(1)
@@ -27,6 +27,32 @@ export const UseEffectExample = () => {
         console.log('Use Effect when counter changed')
         // debugger
         document.title = counter.toString()
+    }, [counter])
+
+    return <div>
+        Hello, counter: {counter} fake: {fake}
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>Counter +
+        </button>
+        <button onClick={() => {
+            setFake(fake + 1)
+        }}>Fake +
+        </button>
+    </div>
+}
+
+export const UseEffectSetTimeoutExample = () => {
+    console.log('UseEffectSetTimeoutExample')
+
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    useEffect(() => {
+        console.log('Use Effect Set Timeout')
+        setTimeout(() => {
+            document.title = counter.toString()
+        }, 3000)
     }, [counter])
 
     return <div>
